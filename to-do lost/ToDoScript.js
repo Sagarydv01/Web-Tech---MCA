0cb337a2a5aa1702f0ce60faa0f014a457cee944
Sagarydv01 <sagydv01@gmail.com>
2026-02-18 15:41:40 +0530

    added functionality of adding task in to-do

---
+++ to-do lost/ToDoScript.js
@@ -0,0 +1,28 @@
+let input = document.querySelector("#task");
+let btn = document.querySelector("#addTask");
+let list = document.querySelector("#listTask");
+
+// console.log(input.value);
+
+btn.addEventListener("click", () => {
+    // console.log(input.value);
+    let task = input.value;
+
+    if (task === "")
+        // console.log("No task added!");
+        alert("No task Added!");
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = task;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        li.appendChild(span);
+        
+        let dlt = document.querySelector(span.parentElement);
+        dlt.addEventListener("click", () => {
+            
+        })
+        
+    } 
+});
